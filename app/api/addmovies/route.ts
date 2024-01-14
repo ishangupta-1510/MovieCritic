ba7707466903bae53bf1fd967a2a17373cf1313b
gupta.ishan.1510 <gupta.ishan.1510@gmail.com>
2024-01-14 15:19:31 +0530

    database connected

--- app/api/addmovies/route.ts
+++ app/api/addmovies/route.ts
@@ -1,10 +1,27 @@
-import prisma from "@/prisma";
+import { db } from "@/lib/db";
+
 import { NextResponse } from "next/server";
 
-export async function main() {
+export async function POST(req: Request) {
   try {
-    await prisma.$connect();
-  } catch (err) {
-    return Error("Database Connection Unsuccessull");
+    // detsrtucture todoTitle from the incoming request
+    const { movie, releasedate } = await req.json();
+
+    if (!movie) {
+      return new NextResponse("Title required", { status: 400 });
+    }
+
+    // Create and save todo on the database
+    const movies = await db.movie.create({
+      data: {
+        title: movie,
+        releaseDate: releasedate,
+      },
+    });
+
+    return NextResponse.json(movies, { status: 200 }); // Respond with the created todo
+  } catch (error) {
+    console.log("movies", error);
+    return new NextResponse("Internal Server Error", { status: 500 }); // Handle errors
   }
 }
